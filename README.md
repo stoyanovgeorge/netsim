03651704e137d80ee235b322ba8f36125f4e7787
stoyanovgeorge <gstoyanov@gmail.com>
2018-05-17 11:13:33 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,74 @@
+# Network impairments simulator
+
+### Introduction
+
+A simple script for simulating network impairments such as delay, packet loss and jitter. 
+
+### Prerequisites
+
+My script is based on the iproute package which usually comes preinstalled with all modern Linux distributions. In case it is not installed on your machine you can install it easily using the package manager or compile it from [source](https://github.com/shemminger/iproute2):
+```
+git clone https://github.com/shemminger/iproute2.git
+```
+A good guide of the iproute functionality could be find [here](https://wiki.linuxfoundation.org/networking/netem "IPROUTE Usage Guide") 
+
+### Usage examples
+
+You can run the software by executing the script and filling and defining the Ethernet interface, the time delay and jitter in miliseconds and the packet loss in percentage. Afterwards it asks you for confirmation:
+
+```
+$ ./netem.sh
+Welcome to the network simulator:
+
+Here is a list of your network interfaces:
+
+eno1  XXX.XXX.XXX.XXX
+eno2  XXX.XXX.XXX.XXX
+eno3  XXX.XXX.XXX.XXX
+
+Please select the network interface you want to use:
+eno3
+Please define the time delay in [ms]:
+100
+Please define the jitter in [ms]:
+50
+Please define the packet loss in [%]:
+10
+
+These are the set parameters:
+
+Network interface:      eno3
+Delay:                  100ms
+Jitter:                 50ms
+Packet Loss:            10%
+
+Please confirm that these values are correct [Y/n]:
+y
+
+These are the set parameters:
+
+Network interface:      eno3
+Delay:                  100ms
+Jitter:                 50ms
+Packet Loss:            10%
+
+```
+I have created couple of checks so that the script won't accept invalid network port or packet loss higher than 100%. 
+
+### Similar Software
+
+This script is similar to [WANEM](http://wanem.sourceforge.net/) but more limited in functionality, since it is supporting only packet loss, jitter and time delay. On the other hand I found that WANEM is not working properly on newer hardware.
+
+### Future development
+
+I was thinking to create a simple web Interface and even a docker container which will streamline the deployment.
+
+### Bugs and Missing Features
+
+Please use Github Issues in case you spot a bug or have an idea how to optimize the scripts.
+
+### External Links
+
+* [IPROUTE2](https://github.com/shemminger/iproute2 "IPROUTE Official Github Page") 
+* [IPROUTE Usage Guide](https://wiki.linuxfoundation.org/networking/netem "IPROUTE Usage Guide")
+* [WANEM](http://wanem.sourceforge.net/ "WANEM official website")
---
+++ netsim.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+
+
+# Simulate network delay
+printf "Welcome to the network simulator:\n\n"
+
+function kill_all {
+        # Clearing the netem configuration
+        for i in "${net_array[@]}"
+        do
+                sudo tc qdisc del dev "$i" root &> /dev/null
+        done
+}
+
+function network_array {
+        # Creating an array containing all network interfaces which are up
+        net_array=()
+        for iface in $(ifconfig | cut -d ' ' -f1| tr ':' '\n' | awk NF)
+        do
+                net_array+=("$iface")
+        done
+        unset "net_array[${#net_array[@]}-1]"
+}
+
+function user_input {
+        # Printing available interfaces
+
+        printf "Here is a list of your network interfaces:\n\n"
+
+        for iface in $(ifconfig | cut -d ' ' -f1| tr ':' '\n' | awk NF)
+        do
+                addr=$(ip -o -4 addr list $iface | awk '{print $4}' | cut -d/ -f1)
+                if [[ $iface != "lo" ]]
+                then
+                        printf "$iface""%s\t""$addr""%s\n"
+                fi
+        done
+
+        # Network Interface selection
+
+        printf "\nPlease select the network interface you want to use:\n"
+        read -r user_iface
+
+        while ! [[ "${net_array[*]}" =~ (^| )"$user_iface"( |$) ]]; do # check if the user input is valid
+                echo "Please enter a valid network interface:"
+                read -r user_iface
+        done
+
+        # Time Delay Setup
+
+        echo "Please define the time delay in [ms]:"
+        read -r delay
+        while ! [[ "$delay" =~ ^[0-9]+$ && "delay" -ge 0 ]]; do
+                echo "Please enter a valid numerical time delay value in [ms]:"
+                read -r delay
+        done
+
+        # Jitter Setup
+
+        echo "Please define the jitter in [ms]:"
+        read -r jitter
+
+        while ! [[ "$jitter" =~ ^[0-9]+$ && "$jitter" -ge '0' ]]; do
+                echo "Please enter only a valid integer value for the jitter greater than 0ms:"
+                read -r jitter
+        done
+
+        # Packet Loss Setup
+
+        echo "Please define the packet loss in [%]:"
+        read -r ploss
+
+        while ! [[ "$ploss" =~ ^[0-9]+$ && "$ploss" -lt '100' && "$ploss" -ge '0' ]]; do
+                echo "Please enter a valid packet loss value in [%], between 0% and 100%:"
+                read -r ploss
+        done
+}
+
+function confirmation {
+        read -p "Please confirm that these values are correct [Y/n]:"$'\n' -n 1 choice
+        case "$choice" in
+                y|Y|"" ) ;;
+                n|N ) printf "\nHere we go again:\n"
+                        user_input
+                        print_settings
+                        confirmation;;
+                * ) printf "\nYou have entered invalid input!\n"
+                        confirmation;;
+esac
+}
+
+
+function print_settings {
+
+        printf "\nThese are the set parameters:\n\n"
+        printf "Network interface:\t"$user_iface"\n"
+        printf "Delay:\t\t\t"$delay"ms\n"
+        printf "Jitter:\t\t\t"$jitter"ms\n"
+        printf "Packet Loss:\t\t"$ploss"%%\n\n"
+}
+
+function netemu {
+        sudo tc qdisc add dev "$user_iface" root netem delay "$delay"ms "$jitter"ms loss "$ploss"%
+}
+
+network_array
+kill_all
+user_input
+print_settings
+confirmation
+print_settings
+netemu
